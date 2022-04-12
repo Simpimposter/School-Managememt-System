1faf8c23a6f6ff7117090baa3ad365f45cf8a08f
Simpimposter <shashwatroy70@gmail.com>
2022-04-12 18:52:38 +0530

    checked for numbers

---
+++ src/SchoolManagementSystem/Shashwat.java
@@ -0,0 +1,18 @@
+package SchoolManagementSystem;
+//this class is for keeping track of students fees, name, grade etc.
+
+import java.util.Scanner;
+
+public class Shashwat {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int a=sc.nextInt();
+        if (a >= 2) {
+            System.out.println("ok");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
+
